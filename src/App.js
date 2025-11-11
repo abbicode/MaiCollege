0b0133c25316a20c9c59c4fc640de9e4a523e30e
Abby Goblick <goblicka@gmail.com>
2025-11-11 18:15:28 -0500

    shadcn works

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import TodoList from './components/TodoList';
 import Draggable from 'react-draggable';
 import AutoCompleteDropdown from './components/AutoCompleteDropdown';
-import { Button } from "@/components/ui/button";
+import { Button } from "./components/ui/button";
 
 
 function App() {
@@ -12,16 +12,18 @@ function App() {
   
   return (
     <div className="App">
+      <div className="p-10 border-r-50"> 
+      <Button>It works!</Button>
+    </div>
       {/* First Todo List (Post-it note) */}
       <Draggable>
         <div>
+         
          <TodoList title="To-Do List"  />
         </div>
        
       </Draggable>
-    <div className="p-10">
-      <Button>It works!</Button>
-    </div>
+    
 
       {/* Second Todo List (College To-Do List, non-draggable) */}
       <div id="fixed">
